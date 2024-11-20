76d0641b963bfa240d637c749edfa86bb679f52a
Ghenimaa <ghenima.kadi@etud.univ-jfc.fr>
2024-11-20 15:28:07 +0100

    TP8

--- src/main/java/champollion/UE.java
+++ src/main/java/champollion/UE.java
@@ -2,14 +2,10 @@ package champollion;
 
 public class UE {
     private final String myIntitule;
-
     public UE(String intitule) {
         myIntitule = intitule;
     }
-
     public String getIntitule() {
         return myIntitule;
     }
-
-    
 }
